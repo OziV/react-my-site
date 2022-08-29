a32910e854560ee337f5ce2732e88f878d285dc8
OziV <oz.ziv8@gmail.com>
2022-08-30 01:13:41 +0300

    WIP: update top projects

--- src/components/ProjectsTop.js
+++ src/components/ProjectsTop.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { useTitle } from "../custom_hooks/useTitle";
+import { useFetch } from "../custom_hooks/useFetch";
 import { text_title } from "../data/dataText";
 import CardItem from "../card/CardItem";
 import Loader from "../misc/Loader";
 import { CgSandClock } from "react-icons/cg";
 
 function ProjectsTop() {
+  const { dataSorted, loading } = useFetch(
+    process.env.REACT_APP_TOP_PROJECTS_URL
+  );
   const { title } = useTitle(text_title[1].name);
 
   const GetIcon = () => {
@@ -17,18 +21,18 @@ function ProjectsTop() {
   };
 
   const GetData = () => {
-    // if (loading) {
-    //   return <Loader />;
-    // } else {
-    //   return <CardItem data={data} />;
-    // }
+    if (loading) {
+      return <Loader />;
+    } else {
+      return <CardItem dataSorted={dataSorted} />;
+    }
   };
 
   return (
     <section className="section">
       {title}
       <GetData />
-      <GetIcon />
+      {/* <GetIcon /> */}
     </section>
   );
 }
